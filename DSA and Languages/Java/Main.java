dc565fe615b1c6321a2f9170a648cd6ace65c73d
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-07-30 12:46:20 +0530

    Learning Java ...

--- DSA and Languages/Java/Main.java
+++ DSA and Languages/Java/Main.java
@@ -1,14 +1,27 @@
 class Main {
 
-	public static void main(String[] args)
-	{
-		String name = "Shaurya";
-		short age = 22;
+	public static void main(String[] args) {
+		// decimal (base 10)
+		int i = 9;
+		System.out.println(i); // output: 9
 
-		System.out.print("Name: " + name);
-		System.out.println("Age: " + age);
-		System.out.printf("Name: %s, Age: %d", name, age);
-		System.out.printf("Name: %s, Age: %d", name, age);
+		// binary (base 2)
+		int ib = 0b101;
+		System.out.println(ib); // output: 5
+
+		// octa-decimal (base 8)
+		int io = 012;
+		System.out.println(io); // output: 10
+
+		// hexa-decimal (base 16)
+		int ih = 0x7E;
+		System.out.println(ih); // output: 5
+
+		int ibig = 10_00_00_000;
+		System.out.println(ibig); // output: 100000000
+
+		byte num1 = 100;
+		System.out.println(num1); // output: 100
 
 	}
 }

c2898b5343e1396fd670dec4ddb461405a2b4002
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-03 16:01:45 +0530

    Completed chap 23 of java, startgin with chap 24 ....

--- DSA and Languages/Java/Main.java
+++ DSA and Languages/Java/Main.java
@@ -1,22 +1,13 @@
-class Parent {
-	public Parent() {
-		System.out.println("Parent constructor");
-	}
-}
-
-class Child extends Parent {
-	public Child(int value) {
-		super();
-		System.out.println("Child constructor with value: " + value);
-	}
+@FunctionalInterface
+interface Printer {
+	void print(String message);
 }
 
 public class Main {
 	public static void main(String[] args) {
-		// This will cause a compile-time error
-		// Parent parent = new Child(); 
-		
-		// Also not possible without appropriate constructor
-		Parent parent = new Child(10); 
+		// Method reference to an instance method
+		Printer printer = System.out::println;
+
+		printer.print("Hello, World"); // Outputs: Hello, World
 	}
 }

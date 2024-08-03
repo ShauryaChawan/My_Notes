2045f85316f6076b1dfe3defd9e8849a3a163dde
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-03 23:13:57 +0530

    Completed chap 24 of java, startgin with chap 25 ....

--- DSA and Languages/Java/Main.java
+++ DSA and Languages/Java/Main.java
@@ -1,12 +1,23 @@
-// Functional interface
-@FunctionalInterface
-interface MathOperation {
-	int operate(int a, int b);
-}
+enum Direction {
+	NORTH(0), EAST(90), SOUTH(180), WEST(270);
 
-public class Main {
-	public static void main(String[] args) {
-		MathOperation addition = (a, b) -> a + b;
-		System.out.println(addition.operate(5, 3)); // Outputs: 8
+	private int degrees;
+
+	// Static field
+	private static final int TOTAL_DIRECTIONS = 4;
+
+	// Constructor
+	Direction(int degrees) {
+		this.degrees = degrees;
+	}
+
+	// Getter method
+	public int getDegrees() {
+		return degrees;
+	}
+
+	// Static method
+	public static int getTotalDirections() {
+		return TOTAL_DIRECTIONS;
 	}
 }

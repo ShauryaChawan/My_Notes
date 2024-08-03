cd13edc9ce429c04b9d331116291bc6fcb9b9332
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-03 14:14:24 +0530

    Completed chap 20 of java, startgin with chap 21 ....

--- DSA and Languages/Java/Main.java
+++ DSA and Languages/Java/Main.java
@@ -1,31 +1,19 @@
-// Java Program to Illustrate Copy Constructor
-
-// Class 1
-class Complex {
-
-	// Class data members
-	private double re, im;
-
-	// Constructor
-	public Complex(double re, double im) {
-		// this keyword refers to current instance itself
-		this.re = re;
-		this.im = im;
-	}
+interface Greeting {
+	void greet();
 }
 
-// Class 2
-// Main class
 public class Main {
-
-	// Main driver method
 	public static void main(String[] args) {
+		// Anonymous class implementing Greeting interface
+		Greeting greeting = new Greeting() {
+			@Override
+			public void greet() {
+				System.out.println("Hello, Anonymous!");
+			}
+		};
 
-		// Creating object of above class
-		// inside main() method
-		Complex c1 = new Complex(10, 15);
-
-		// Note: compiler error here
-		Complex c2 = new Complex(c1);
+		greeting.greet(); 
 	}
 }
+
+// Outputs: Hello, Anonymous!

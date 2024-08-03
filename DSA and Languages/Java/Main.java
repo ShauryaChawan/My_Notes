ae655523322ac980439f39a4a5e04d5db2208a30
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-03 12:24:36 +0530

    Updated chap 15, added 'Copy constructor' concept...

--- DSA and Languages/Java/Main.java
+++ DSA and Languages/Java/Main.java
@@ -1,12 +1,31 @@
-class Animal {
-	public void makeSound() {
-		System.out.println("Some generic animal sound");
+// Java Program to Illustrate Copy Constructor
+
+// Class 1
+class Complex {
+
+	// Class data members
+	private double re, im;
+
+	// Constructor
+	public Complex(double re, double im) {
+		// this keyword refers to current instance itself
+		this.re = re;
+		this.im = im;
 	}
 }
 
-class Dog extends Animal {
-	@Override
-	public void makeSound() {
-		System.out.println("Bark");
+// Class 2
+// Main class
+public class Main {
+
+	// Main driver method
+	public static void main(String[] args) {
+
+		// Creating object of above class
+		// inside main() method
+		Complex c1 = new Complex(10, 15);
+
+		// Note: compiler error here
+		Complex c2 = new Complex(c1);
 	}
 }

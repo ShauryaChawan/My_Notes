cf58a13aeef1144339ed9192697d1d3e07e08a96
Shaurya Chawan <shauryachawan4646.dev@gmail.com>
2024-08-05 01:04:47 +0530

    Updated files

--- DSA and Languages/Java/Main.java
+++ DSA and Languages/Java/Main.java
@@ -1,22 +1,5 @@
-abstract class A {
-  public abstract void show();
-}
-
 public class Main {
   public static void main(String[] args) {
     
-    /* Following line throws an error, 
-    as abstract classes cannot be instantiated or 
-    we cannot create an object of abstrract classes
-    */
-    // A obj = new A();
-
-    // but following code works
-    A obj = new A(){
-      public void show(){
-        System.out.println("in A show");
-      }
-    };
-    obj.show(); // Output: in A show
   }
 }
